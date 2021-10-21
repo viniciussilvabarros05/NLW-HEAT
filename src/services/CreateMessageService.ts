4ed3afc25a4d7d25eed23f24b26ba097c386c6dd
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2021-10-21 08:06:54 -0300

    created on prisma a new table

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,12 @@
+import prismaClient from "../prisma";
+
+class CreateMessageService{
+    async execute (text: string, user_id: string ){
+        const message = await prismaClient.message.create({
+            data:{
+                text,
+                user_id
+            }
+        })
+    }
+}
